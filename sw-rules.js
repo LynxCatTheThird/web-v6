f277cb6b05c330192977b5e97c3d282bcfb82145
LynxCatTheThird <tsfxlyx@qq.com>
2024-05-18 09:41:13 +0800

    chore: 合并 Hexo 7 的 _config.yml chore: 执行发布 chore: 拉取主题 fix: 调整 sw-rules.js replacer 以修复 Vercel 拉取出错问题 fix: 调整 sw-rules.js cacheRules 以适配 CDN 配置 fix: 移除 sw-rules.js 中已弃用函数 perf: 优化 CDN

--- sw-rules.js
+++ sw-rules.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     serviceWorker: {
         escape: 0,
-        cacheName: 'LPTFBlogCache'
+        cacheName: 'ExampleBlogCache'
     },
     register: {
         onsuccess: undefined,
@@ -26,7 +26,7 @@ module.exports.config = {
             caches.match(location.href).then(res => {
                 if (res)
                     res.json().then(json => {
-                        utils && utils.snackbarShow(`已刷新缓存，更新为${json.global + '.' + json.local}版本最新内容`, false, 2000)
+                        console.log(`已刷新缓存，更新为${json.global + '.' + json.local}版本最新内容`);
                     });
                 else
                     console.info('未找到缓存');
@@ -44,20 +44,10 @@ module.exports.config = {
     },
     external: {
         timeout: 5000,
-        concurrencyLimit: 100,
+        concurrencyLimit: 10,
         js: [],
         stable: [],
-        replacer: srcUrl => {
-            if (srcUrl.startsWith('https://cdn.jsdelivr.net/')) {
-                const pathname = new URL(srcUrl).pathname;
-                return [
-                    srcUrl,
-                    `https://jsd.cdn.zzko.cn/${pathname}`,
-                ];
-            } else {
-                return srcUrl;
-            }
-        }
+        replacer: srcUrl => srcUrl,
     }
 };
 
@@ -65,17 +55,17 @@ module.exports.cacheRules = {
     simple: {
         clean: true,
         search: false,
-        match: (url, $eject) => (url.host.includes('lynx') || url.host.includes('lctt')) && url.pathname.match(/(\.(js|css|xml|json)|\/)$/) // 主站缓存
+        match: (url, $eject) => (url.host.includes('lynx') || url.host.includes('lctt')) && url.pathname.match(/(\.(js|css|xml|json)|\/)$/) && !url.host.includes('api') // 主站缓存
     },
     cdn: {
         clean: true,
         match: url => [
-            // "cdn.staticfile.net",
-            // "cdn.bootcdn.net",
-            "jsd.cdn.zzko.cn",
+            "cdn.staticfile.org",
+            "cdn.staticfile.net",
+            "evan.beee.top",
             // "jsd.onmicrosoft.cn",
-            "sdk.51.la",
-            "www.clarity.ms",
+            // "sdk.51.la",
+            // "www.clarity.ms",
             // "s4.zstatic.net",
             "s2.hdslb.com",
             // "fonts.googleapis.com",
